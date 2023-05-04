d0fa90f13581860d2d2fb636012dad64a97d3643
sokhiennn <22013179@myrp.edu.sg>
2023-05-04 14:06:14 +0800

    completed up to section E

--- app/src/main/java/sg/edu/rp/c346/id22013179/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22013179/week03/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -14,8 +15,9 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDisplay;
     Button btnDisplay;
     EditText etInput;
-
     ToggleButton tbtn;
+
+    RadioGroup rgGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +28,22 @@ public class MainActivity extends AppCompatActivity {
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
         tbtn = findViewById(R.id.toggleButtonEnabled);
-
+        rgGender = findViewById(R.id.RadioGroupGender);
         btnDisplay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Code for the action
                 String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+                int checkedRadioId = rgGender.getCheckedRadioButtonId();
+                if(checkedRadioId == R.id.radioButtonGenderMale) {
+                    // Write the code when male selected
+                    stringResponse = "He says " + stringResponse;
+                }else{
+                    // Write the code when female selected
+                    stringResponse = "She says " + stringResponse;
+                }
                 tvDisplay.setText(stringResponse);
             }
         });
